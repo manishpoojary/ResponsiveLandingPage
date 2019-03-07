aaf99b989f06678edbb44f3b63a665ea6b3c0139
devteam94 <devteam94@gmail.com>
2019-03-07 17:48:33 +0530

    Added Nav Hover Animation

---
+++ main.js
@@ -0,0 +1,44 @@
+
+$(function() {
+  var $el,
+    leftPos,
+    newWidth,
+    $mainNav = $(".navbar-nav");
+
+  $mainNav.append("<li id='magic-line'></li>");
+  var $magicLine = $("#magic-line");
+
+  $magicLine
+    .width($(".active").width())
+    .css("left", $(".active a").position().left)
+    .data("origLeft", $magicLine.position().left)
+    .data("origWidth", $magicLine.width());
+
+  $(".navbar-nav li a").hover(
+    function() {
+      $el = $(this);
+      leftPos = $el.position().left;
+      newWidth = $el.parent().width();
+      $magicLine.stop().animate({
+        left: leftPos,
+        width: newWidth
+      });
+    },
+    function() {
+      $magicLine.stop().animate({
+        left: $magicLine.data("origLeft"),
+        width: $magicLine.data("origWidth")
+      });
+    }
+  );
+
+  $('nav a').click(function(){
+    //$(yourSelector).removeClass('enable');
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 700, function(){
+       // $(yourSelector).addClass('enable');
+    });
+    return true;
+});
+});
